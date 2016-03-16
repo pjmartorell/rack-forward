a5fc16d6bd673cca29bb813f99243d1060a45b98
Brian Walsh <brian@bwalsh.com>
2016-03-15 17:52:26 -0700

    fix content length

--- lib/rack/forward.rb
+++ lib/rack/forward.rb
@@ -34,7 +34,8 @@ module Rack
       end
 
       sub_request.content_length = req.content_length
-
+      puts "**** sub_request.content_length:#{sub_request.content_length}"
+      
       if req.content_type
         sub_request.content_type = req.content_type
       end
